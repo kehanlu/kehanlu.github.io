befac98727747c04041d7ebbb09e54d71483c32e
KeHan Lu <kehanluu@gmail.com>
2020-11-29 22:07:06 +0800

    西元2020年11月29日 (週日) 22時07分06秒 CST

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,11 +12,11 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt',
+    title: 'Hank Lu',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'About Hank Lu' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
