992171370bc41b44afae18b2526aabfe50cafd7b
KeHan Lu <kehanluu@gmail.com>
2021-02-28 23:13:11 +0800

    Sun 28 Feb 2021 11:13:11 PM CST

--- pages/index.vue
+++ pages/index.vue
@@ -2,43 +2,44 @@
   <section class="hero">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <img style="border-radius: 50%" width="128px" src="avatar.jpg" />
+        <!-- <img style="border-radius: 50%" width="128px" src="avatar.jpg" /> -->
         <h1 class="title is-spaced is-3" style="margin-bottom: 20px">
           KeHan Lu
         </h1>
       </div>
+      <!-- Introduction -->
       <div class="container">
         <div class="columns is-centered">
           <div class="column is-7">
             <div class="content">
               <p>
                 Hi, I am Hank, a computer science student at TaiwanTech, Taiwan.
-                I love coding and ramen🍜. Use Python to build cool things! My
-                research is about Natural Language Processing with deep learning
-                models. I am also interested in building web applications and
-                information security.
+                I love coding and ramen🍜. My research interest is deep learning
+                and NLP.
               </p>
             </div>
           </div>
         </div>
       </div>
+      <!-- Some Links -->
       <div class="container">
-        <div class="columns is-centered">
+        <div class="columns is-centered links">
           <div class="column is-7">
             <ul>
               <li>
-                <a href="https://me.hanklu.tw">About me</a>: My skills,
-                experiences and projects.
+                <a href="https://me.hanklu.tw">About me</a>
               </li>
-              <li><a href="https://blog.hanklu.tw">Blog</a>: My thoughts.</li>
+              <li><a href="https://blog.hanklu.tw">Blog</a></li>
               <li>
-                <a href="https://no.hanklu.tw">Wiki</a>: My personal knowledge
-                base.
+                <a href="https://www.instagram.com/ramenpie5566/"
+                  >Ramen Instagram</a
+                >
               </li>
             </ul>
           </div>
         </div>
       </div>
+      <!-- Icon -->
       <div class="container">
         <div class="columns is-centered">
           <div class="column has-text-centered">
@@ -65,6 +66,21 @@
           </div>
         </div>
       </div>
+      <!-- more -->
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-7 more-about-me">
+            <strong>More About me</strong>
+            <ul>
+              <li>在 臺科大自然語言處理實驗室 擔任 菸酒生</li>
+              <li>覺得寫程式就是在使用魔法，寫了很多小工具讓生活變得更懶惰</li>
+              <li>最喜歡的語言是 Python</li>
+              <li>習慣把所有事都弄出 SOP 和文件</li>
+              <li>喜歡吃拉麵、去河濱公園騎腳踏車</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -72,6 +88,16 @@
 <style>
 body {
   font-family: "Nunito", sans-serif;
+}
+.links li {
+  margin: 0 1rem;
+  border-bottom: 1px solid;
+  display: inline;
+}
+.more-about-me li {
+  font-size: 0.8rem;
+  list-style: disc;
+  padding-bottom: 0.3rem;
 }
 </style>
 
