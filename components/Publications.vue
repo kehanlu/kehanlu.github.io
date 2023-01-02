35a845cebd543bbfbb46d36aea4b8ed40e619660
kehanlu <kehanluu@gmail.com>
2023-01-02 18:35:21 +0800

    Mon 02 Jan 2023 06:35:21 PM CST

---
+++ components/Publications.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <ol reversed>
+      <!--  -->
+      <li>
+        <ul>
+          <li>
+            <b>
+              A Context-aware Knowledge Transferring Strategy for CTC-based
+              ASR
+            </b>
+          </li>
+          <li>
+            <u>Ke-Han Lu</u> and Kuan-Yu Chen
+          </li>
+          <li>IEEE Spoken Language Technology Workshop (SLT 2022)SLT 2022</li>
+          <li>
+            <a class="tag is-info is-light" href="https://arxiv.org/abs/2210.06244">Paper</a>
+            <a
+              class="tag is-info is-light"
+              href="https://github.com/kehanlu/Mandarin-Wav2Vec2"
+            >GitHub</a>
+          </li>
+        </ul>
+      </li>
+      <!--  -->
+      <li>
+        <ul>
+          <li>
+            <b>Non-autoregressive ASR Modeling using Pre-trained Language Models for Chinese Speech Recognition</b>
+          </li>
+          <li>
+            Fu-Hao Yu, Kuan-Yu Chen, and
+            <u>Ke-Han Lu</u>
+          </li>
+          <li>IEEE/ACM Transactions on Audio, Speech, and Language Processing</li>
+          <li>
+            <a
+              class="tag is-info is-light"
+              href="https://ieeexplore.ieee.org/document/9755057/"
+            >Paper</a>
+          </li>
+        </ul>
+      </li>
+      <!--  -->
+      <li>
+        <ul>
+          <li>
+            <b>A Transformer-based Cross-modal Fusion Model with Adversarial Training for VQA Challenge 2021</b>
+          </li>
+          <li>
+            <u>Ke-Han Lu</u>, Bo-Han Fang and Kuan-Yu Chen
+          </li>
+          <li>Poster spotlight, VQA workshop, CVPR 2021</li>
+          <li>
+            <a class="tag is-info is-light" href="https://arxiv.org/abs/2106.13033">Paper</a>
+            <a class="tag is-info is-light" href="https://visualqa.org/roe.html">Leaderboard</a>
+            <a class="tag is-info is-light" href="https://youtu.be/dYeS8T19ves">Video</a>
+          </li>
+        </ul>
+      </li>
+      <!--  -->
+      <li>
+        <ul>
+          <li>
+            <b>ntust-nlp-2 at ROCLING-2021 Shared Task: BERT-based semantic analyzer with word-level information</b>
+          </li>
+          <li>
+            <u>Ke-Han Lu</u> and Kuan-Yu Chen
+          </li>
+          <li>ROCLING 2021: Conference on Computational Linguistics and Speech Processing</li>
+          <li>
+            <a
+              class="tag is-info is-light"
+              href="https://aclanthology.org/2021.rocling-1.47.pdf"
+            >Paper</a>
+          </li>
+        </ul>
+      </li>
+      <!--  -->
+      <li>
+        <ul>
+          <li>
+            <b>A Preliminary Study of Formosa Speech Recognition Challenge 2020 – Taiwanese ASR</b>
+          </li>
+          <li>
+            Fu-Hao Yu,
+            <u>Ke-Han Lu</u>
+            , Yi-Wei Wang, Wei-Zhe Chang, Wei-Kai Huang and Kuan-Yu Chen
+          </li>
+          <li>International Journal of Computational Linguistics and Chinese Language Processing</li>
+          <li>
+            <a
+              class="tag is-info is-light"
+              href="https://aclanthology.org/2021.ijclclp-1.3.pdf"
+            >Paper</a>
+          </li>
+        </ul>
+      </li>
+    </ol>
+  </div>
+</template>
+<style scoped>
+ol {
+  margin-left: 0.5em;
+}
+ul {
+  list-style-type: none;
+  margin-left: 0.5em;
+}
+</style>
